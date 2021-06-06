b7874cd83e388033aa5f9e281a65abf3999aeffa
Lateralus138 <faithnomoread@yahoo.com>
2021-06-06 15:15:17 -0500

    Attempt to fix permissions on main executable, specifically for use in the AppImage: Sun 06 Jun 2021 03:15:17 PM CDT

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-chmod +x ./appimagetool-x86_64.AppImage
-./appimagetool-x86_64.AppImage src/linux/src/appimage/mcchunkget3cli.AppDir src/linux/target/appimage/mcchunkgetcli-x86_64.AppImage
-chmod +x src/linux/target/appimage/mcchunkgetcli-x86_64.AppImage
+chmod 0755 ./appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage src/linux/src/appimage/mcchunkget3cli.AppDir src/linux/target/appimage/mcchunkgetcli-x86_64.AppImage &&
+chmod 0755 src/linux/target/appimage/mcchunkgetcli-x86_64.AppImage
